4eff52407e4d51b61661580d074c095b765d9ce1
royleguiza <leguizavictorroy@gmail.com>
2026-08-21 19:32:11 +0000

    Hito 0: alinear package/applicationId a com.royleguiza.voicebubblestt (decisión documentada)

--- voice_bubble_stt/android/app/build.gradle.kts
+++ voice_bubble_stt/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.royleguiza.voice_bubble_stt"
+    namespace = "com.royleguiza.voicebubblestt"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.royleguiza.voice_bubble_stt"
+        applicationId = "com.royleguiza.voicebubblestt"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
