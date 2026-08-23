36fd1f19e3bad40afb7cfebebea8ac7573d81307
royleguiza <leguizavictorroy@gmail.com>
2026-08-23 20:14:17 +0000

    fix(test): profundidad de import del helper en widget_test

--- app_source/test/widget_test.dart
+++ app_source/test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:voice_bubble_stt/main.dart';
 
-import '../helpers/mock_channels.dart';
+import 'helpers/mock_channels.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
