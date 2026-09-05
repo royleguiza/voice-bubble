8baed9be2214a050fcc1b6780862220f357e3b39
royleguiza <leguizavictorroy@gmail.com>
2026-09-05 13:59:20 -0300

    Agrega import ViewGroup faltante en BubbleHistoryController

--- voice_bubble_stt/android/app/src/main/kotlin/com/royleguiza/voicebubblestt/BubbleHistoryController.kt
+++ voice_bubble_stt/android/app/src/main/kotlin/com/royleguiza/voicebubblestt/BubbleHistoryController.kt
@@ -16,6 +16,7 @@ import android.view.Gravity
 import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.ImageView
