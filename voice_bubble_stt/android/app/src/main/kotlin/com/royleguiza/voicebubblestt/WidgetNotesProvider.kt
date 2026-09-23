3cd5aebf23e748c879358bc01bdeaecfbc2578c1
royleguiza <leguizavictorroy@gmail.com>
2026-09-23 02:52:49 +0000

    fix(widget): import ComponentName faltante en WidgetNotesProvider

--- voice_bubble_stt/android/app/src/main/kotlin/com/royleguiza/voicebubblestt/WidgetNotesProvider.kt
+++ voice_bubble_stt/android/app/src/main/kotlin/com/royleguiza/voicebubblestt/WidgetNotesProvider.kt
@@ -3,6 +3,7 @@ package com.royleguiza.voicebubblestt
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.view.View
